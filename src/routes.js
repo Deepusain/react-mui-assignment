076f736d6d035d6d0fde458370d69e6f81ea3649
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2025-02-05 23:50:01 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -19,25 +19,3 @@ const AppRoutes = () => {
 };
 
 export default AppRoutes;
-
-// import { Routes, Route, Navigate } from "react-router-dom";
-// import { useAuth } from "./context/AuthContext";
-// import Login from "./components/Login";
-// import Dashboard from "./components/Dashboard";
-
-// const PrivateRoute = ({ children }) => {
-//     const { isAuthenticated } = useAuth();
-//     return isAuthenticated ? children : <Navigate to="/login" />;
-// };
-
-// const AppRoutes = () => {
-//     return (
-//         <Routes>
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-//             <Route path="*" element={<Navigate to="/login" />} />
-//         </Routes>
-//     );
-// };
-
-// export default AppRoutes;
